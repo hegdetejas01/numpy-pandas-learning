702802863c62585990ad09c2bd2ae8ffda1e4ca9
hegdetejas01 <hegde.tejas01@gmail.com>
2026-07-03 17:01:45 +0530

    pandas7 added

--- pandas8.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-import pandas as pd
-
-confirmed = pd.read_csv('datasets/time_series_covid19_confirmed_global.csv') ### data in wide format
-death = pd.read_csv('datasets/time_series_covid19_deaths_global.csv') ### data in wide format
