76f3f6b84a0b6dabc168a7ad0055558535c70625
hegdetejas01 <hegde.tejas01@gmail.com>
2026-07-13 17:13:07 +0530

    pandas 8 edit

--- pandas8.py
+++ pandas8.py
@@ -4,10 +4,6 @@ Original file is located at
     
 """
 
-"""
-1. add a print statement to print everthing
-"""
-
 ############################ PANDAS STRING ############################
 
 ## Vectorised Operations on Strings
@@ -16,133 +12,91 @@ import numpy as np
 import pandas as pd
 
 
-
 # Vextorised implementaion in normal python
 s = ['cat','mat',None,'rat']
 s_c = [i for i in s if i is not None and i.startswith('c')] # words starting with letter c
-s_c
+print(s_c)
 
 s_c = [i.startswith('c') for i in s if i is not None]
-s_c
-
+print(s_c)
 
 
 ## None data or missing data can be handled in normal python
 ## List Comprehension is very slow
 ## Therefore pandas is used for vectorised implementation
 
-
-
-s = pd.Series(['cat','mat',None,'rat'])
-s.str.startswith('c')
 # str is called as accessor
 
-
-
+s = pd.Series(['cat','mat',None,'rat'])
+print(s.str.startswith('c'))
 
 
 titanic = pd.read_csv('datasets/titanic.csv')
 titanic.columns = [i.lower() for i in titanic.columns]
 
-titanic.head()
-
-titanic['name']
-
-titanic['name'].str.lower()
-
-titanic['name'].str.upper()
-
-
-
-titanic['name'].str.title()
-
-titanic['name'].str.capitalize()
-
+print(titanic.head())
+print(titanic['name'])
+print(titanic['name'].str.lower())
+print(titanic['name'].str.upper())
+print(titanic['name'].str.title())
+print(titanic['name'].str.capitalize())
 
 
 # fetch the name which is large in lenght
 i = titanic['name'].str.len().sort_values(ascending=False).head(1).index[0]
-titanic.iloc[i]['name']
+print(titanic.iloc[i]['name'])
 
 # fetch the name which is large in lenght
-titanic['name'][(titanic['name'].str.len()) == (titanic['name'].str.len().max())].values[0]
-
-
-
+print(titanic['name'][(titanic['name'].str.len()) == (titanic['name'].str.len().max())].values[0])
 
 
 # strip
-"               hello world                                               ".strip()
-
-titanic['name'].str.strip()
-
+print("               hello world                                               ".strip())
+print(titanic['name'].str.strip())
 
 
 # split
+titanic['lastname'] = titanic['name'].str.split(',').str.get(0) # get - takes the value in index (index is given : here-0)
+print(titanic.head(2))
 
-titanic['lastname'] = titanic['name'].str.split(',').str.get(0) # get - takes the value in index (index is gives : eg-0)
-
-titanic['name'].str.split(',').str.get(1).str.strip().str.split(" ", n=1, expand=True)
+print(titanic['name'].str.split(',').str.get(1).str.strip().str.split(" ", n=1, expand=True))
 # n=1 specifies that i need just one split [ between Mr. and Name ]
 # expand = True, creates a DF with splited words
 
 titanic[['title','firstname']] = titanic['name'].str.split(',').str.get(1).str.strip().str.split(" ", n=1, expand=True)
-
-titanic.head(2)
+print(titanic.head(2))
 
 # fetch counts on titles
-titanic['title'].value_counts()
-
-
-
+print(titanic['title'].value_counts())
 
 
 ## replace
 # ms, miss, mlle are the same title = miss
-
 titanic['title'] = titanic['title'].str.replace('Ms.', 'Miss.') # replace Ms. with Miss.
 titanic['title'] = titanic['title'].str.replace('Mlle.', 'Miss.') # replace Mlle. with Miss.
-titanic['title'].value_counts()
-
-
-
+print(titanic['title'].value_counts())
 
 
 ### filtering
 # get the name of the passengers whose first name starts with A
-titanic['name'][titanic['firstname'].str.startswith('A')]
+print(titanic['name'][titanic['firstname'].str.startswith('A')])
 
-titanic['name'][titanic['firstname'].str.endswith('A')]
-
-
-
-titanic[titanic['firstname'].str.isdigit()]
-
-
-
-
-
+print(titanic['name'][titanic['firstname'].str.endswith('A')])
+print(titanic[titanic['firstname'].str.isdigit()])
 
 
 ##### applying regex
-
 # names of all passengers whose name contains john (not case sensitive)
-titanic['name'][titanic['firstname'].str.contains('john', case=False)]
+print(titanic['name'][titanic['firstname'].str.contains('john', case=False)])
 
 # extract names of passengers whose lastname start with vowels and end with vowels
-titanic[['lastname','passengerid','name']][titanic['lastname'].str.contains('^[aeiouAEIOU].+[aeiouAEIOU]$')].set_index('passengerid')
+print(titanic[['lastname','passengerid','name']][titanic['lastname'].str.contains('^[aeiouAEIOU].+[aeiouAEIOU]$')].set_index('passengerid'))
 
 # extract names of passengers whose lastname start with consonents and end with consonents
-titanic[['lastname','passengerid','name']][titanic['lastname'].str.contains('^[^aeiouAEIOU].+[^aeiouAEIOU]$')].set_index('passengerid')
-
-
-
-
-
+print(titanic[['lastname','passengerid','name']][titanic['lastname'].str.contains('^[^aeiouAEIOU].+[^aeiouAEIOU]$')].set_index('passengerid'))
 
 
 ### Slicing
-
-titanic['name'].str[:4] # gives the first 4 char
-titanic['name'].str[::-1] # reverse the name
-titanic['name'].str[::2] # alternate char of the name
+print(titanic['name'].str[:4]) # gives the first 4 char
+print(titanic['name'].str[::-1]) # reverse the name
+print(titanic['name'].str[::2]) # alternate char of the name
